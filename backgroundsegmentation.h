c9bd43b32192dbc0aab97cf8daef6b1b8acc81b2
sapo <00sapo@anche.no>
2017-04-08 10:24:01 +0200

    Added header for BackgroudSegmetation. To be implemeted.

--- backgroundsegmentation.h
+++ backgroundsegmentation.h
@@ -1,11 +1,45 @@
 #ifndef BACKGROUNDSEGMENTATION_H
 #define BACKGROUNDSEGMENTATION_H
+#include <opencv2/opencv.hpp>
 
-
-class BackgroundSegmentation
-{
+/**
+ * @brief The BackgroundSegmentation class performs the removing of background from a RGB-D image.
+ */
+class BackgroundSegmentation {
 public:
     BackgroundSegmentation();
+
+    /**
+     * @brief setImageCollection: set the collection of images to be filter
+     * @param imageSequence: A vector containing the collection (i.e. the one given by ImageLoader)
+     */
+    void setImageCollection(std::vector<cv::Mat>& imageSequence);
+
+    /**
+     * @brief findTreshold: Automatically find the threshold to be used for filtering
+     * @return A double containg the threshold value
+     */
+    double findTreshold();
+
+    /**
+     * @brief filter: Remove the background in each image of the collection using the threshold
+     * @return return True if everything worked, false otherwise.
+     */
+    bool filter();
+
+    /**
+     * @brief filter: set the threshold and calls filter()
+     * @param threshold: the value to use as threshold
+     * @return same as filter
+     */
+    bool filter(double threshold);
+
+    double getTreshold() const;
+    void setTreshold(double value);
+
+private:
+    std::vector<cv::Mat> imageCollection;
+    double treshold;
 };
 
 #endif // BACKGROUNDSEGMENTATION_H
