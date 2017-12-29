56b734799d4eef17865ecc16f4d2ba5b6c6570af
Federico Simonetta <info@federicosimonetta.it>
2017-12-29 17:22:33 +0100

    implemented loading based on actual directories

--- src/main.cpp
+++ src/main.cpp
@@ -3,12 +3,16 @@
 
 #include <opencv2/highgui/highgui.hpp>
 
+#include "boost/regex.hpp"
 #include "datasetcov.h"
 #include "facerecognizer.h"
 #include "image4dloader.h"
 #include "preprocessor.h"
 #include "test.h"
+#include <experimental/filesystem>
 
+using namespace std::experimental::filesystem;
+using namespace boost;
 using cv::Mat;
 using std::string;
 using std::vector;
@@ -17,15 +21,15 @@ using namespace face;
 const int SUBSETS = 3;
 
 const cv::String KEYS = "{ help h usage ?           | | print this message }"
-                        "{ training                 | | load images from dataset    }"
-                        "{ preprocessedTrainingset  | | load images from preprocessed dataset }"
+                        "{ trainingset              | | load images from training set    }"
+                        "{ preprocessedTrainingset  | | load images from preprocessed training set }"
                         "{ saveTrainingset          | | if set saves preprocessed images }"
                         "{ query                    | | path to query identity }"
                         "{ saveTrained              | | path to store trained svms }"
                         "{ loadTrained              | | path to trained svms }"
-                        "{ testingset               | | path to testing set }"
+                        "{ testingset               | | load images from testing set }"
                         "{ saveTestingset           | | if set saves preprocessed testing set }"
-                        "{ preprocessedTestingset   | | path to preprocessed testing set }";
+                        "{ preprocessedTestingset   | | load images from preprocessed testing set }";
 
 void testFunctions();
 DatasetCov loadAndPreprocess(const string& datasetPath, std::size_t covarianceSubsets);
@@ -54,7 +58,7 @@ int proxyDatasetLoader(string type, cv::CommandLineParser parser, DatasetCov dat
             std::cout << "Warning! Loaded inconsistent dataset!" << std::endl;
         if (dataset.empty()) {
             std::cout << "Error! Loaded empty dataset!" << std::endl;
-            return 0;
+            return 1;
         }
     } else if (parser.has(optionDataset)) {
 
@@ -67,13 +71,14 @@ int proxyDatasetLoader(string type, cv::CommandLineParser parser, DatasetCov dat
                 std::cerr << "Error saving preprocessed dataset to " << path << std::endl;
         }
     }
+    return 0;
 }
 int main(int argc, char* argv[])
 {
     cv::CommandLineParser parser(argc, argv, KEYS); // opencv parser for command line arguments
 
     // if wrong arguments, print usage
-    if (!parser.has("dataset") && !parser.has("preprocessedDataset")) {
+    if (!parser.has("trainingset") && !parser.has("preprocessedTrainingset")) {
         parser.printMessage();
         return 0;
     }
@@ -106,23 +111,31 @@ DatasetCov loadAndPreprocess(const string& datasetPath, std::size_t covarianceSu
 
     Preprocessor preproc;
     vector<vector<Mat>> grayscale, depthmap;
-    for (int i = 1; i < 25; ++i) { // TODO: load each folder using std::experimental::file_system
-        std::cout << "Identity " << i << std::endl;
-        auto path = datasetPath + "/" + (i < 10 ? "0" : "") + std::to_string(i);
-        loader.setCurrentPath(path);
+    vector<string> files;
+    regex expr{ ".*/[0-9][0-9]" };
+    int id = 0;
+    if (is_directory(datasetPath)) {
+        for (auto& x : directory_iterator(datasetPath)) {
+            string path = x.path().string();
+            if (is_directory(path) && regex_match(path, expr)) {
+                std::cout << "Identity " << id << std::endl;
+                loader.setCurrentPath(path);
 
-        std::cout << "Loading and preprocessing images..." << std::endl;
-        auto faces = loader.get();
-        //for (const auto &face : faces) {
-        //    cv::imshow("face",face.image);
-        //    cv::waitKey();
-        //}
-        auto preprocessedFaces = preproc.preprocess(faces);
-        std::cout << "Computing covariance representation..." << std::endl;
-        vector<Mat> grayscaleCovar, depthmapCovar;
-        covariance::getNormalizedCovariances(preprocessedFaces, covarianceSubsets, grayscaleCovar, depthmapCovar);
-        grayscale.push_back(std::move(grayscaleCovar));
-        depthmap.push_back(std::move(depthmapCovar));
+                std::cout << "Loading and preprocessing images..." << std::endl;
+                auto faces = loader.get();
+                //for (const auto &face : faces) {
+                //    cv::imshow("face",face.image);
+                //    cv::waitKey();
+                //}
+                auto preprocessedFaces = preproc.preprocess(faces);
+                std::cout << "Computing covariance representation..." << std::endl;
+                vector<Mat> grayscaleCovar, depthmapCovar;
+                covariance::getNormalizedCovariances(preprocessedFaces, covarianceSubsets, grayscaleCovar, depthmapCovar);
+                grayscale.push_back(std::move(grayscaleCovar));
+                depthmap.push_back(std::move(depthmapCovar));
+            }
+            id++;
+        }
     }
 
     return { std::move(grayscale), std::move(depthmap) };
